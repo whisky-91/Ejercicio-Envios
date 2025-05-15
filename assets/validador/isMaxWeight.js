1e20436e596b4d7ee942a62b9e2157e7b5c1cf8a
adrianbr_Global <adrianbr@global.com>
2025-05-15 20:03:21 +0200

    added function isMaxWeight

---
+++ assets/validador/isMaxWeight.js
@@ -0,0 +1,10 @@
+/**
+ * Represent a isMaxWeight
+ * @param {number} w - Check if weight is more than 5.000
+ * @returns true
+ */
+export function isMaxWeight(w) {
+  if (w > 5000) {
+    return true;
+  }
+}
