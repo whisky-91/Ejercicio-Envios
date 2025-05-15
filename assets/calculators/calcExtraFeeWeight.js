07e2ceca87fbf57cf251c4704bcad1b130ec27b1
adrianbr_Global <adrianbr@global.com>
2025-05-15 20:02:46 +0200

    added function calculator extra fee weight

---
+++ assets/calculators/calcExtraFeeWeight.js
@@ -0,0 +1,8 @@
+/**
+ * Represent a calcExtraWeight
+ * @param {number} w - Calculate the price for the extra weight
+ * @returns number
+ */
+export function calcExtraWeight(w) {
+  return 1.2 * w;
+}
