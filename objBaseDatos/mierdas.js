8804f57d1995e04d546905e0c7582dc8f722a69a
Miguel <mmmmaiv2@gmail.com>
2025-05-15 18:44:58 +0200

    objeto base de datos con distancias

---
+++ objBaseDatos/mierdas.js
@@ -0,0 +1,21 @@
+
+// let ciudad = ciudades.ciudad1;
+
+// let capital = ciudad.nombreCiudad;
+
+// let puerto = ciudad.tienePuerto;
+
+console.log(ciudad);
+console.log(capital);
+console.log(puerto);
+
+for (const k in ciudades) {
+  if (Object.prototype.hasOwnProperty.call(ciudades, k)) {
+    let ciudadN = ciudades[k];
+    console.log({ ciudadN });
+
+    
+  }
+
+  let capital = ciudadN.
+}
