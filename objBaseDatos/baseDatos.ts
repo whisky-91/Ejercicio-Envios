c59ac6f7a708a61c55f15cd05e053f0771de7b83
Miguel <mmmmaiv2@gmail.com>
2025-05-15 17:15:04 +0200

    Objeto Base de Datos Ciudades

---
+++ objBaseDatos/baseDatos.ts
@@ -0,0 +1,74 @@
+type Ciudad = {
+  nombreCiudad: string;
+  tienePuerto: boolean;
+};
+
+// Objeto ciudades actualizado
+const ciudades: { [clave: string]: Ciudad } = {
+  ciudad1: { nombreCiudad: "Vitoria-Gasteiz", tienePuerto: false },
+  ciudad2: { nombreCiudad: "Albacete", tienePuerto: false },
+  ciudad3: { nombreCiudad: "Alicante", tienePuerto: false },
+  ciudad4: { nombreCiudad: "Almería", tienePuerto: false },
+  ciudad5: { nombreCiudad: "Oviedo", tienePuerto: false },
+  ciudad6: { nombreCiudad: "Ávila", tienePuerto: false },
+  ciudad7: { nombreCiudad: "Badajoz", tienePuerto: false },
+  ciudad8: { nombreCiudad: "Barcelona", tienePuerto: false },
+  ciudad9: { nombreCiudad: "Burgos", tienePuerto: false },
+  ciudad10: { nombreCiudad: "Cáceres", tienePuerto: false },
+  ciudad11: { nombreCiudad: "Cádiz", tienePuerto: true }, // Sur
+  ciudad12: { nombreCiudad: "Castellón de la Plana", tienePuerto: false },
+  ciudad13: { nombreCiudad: "Ciudad Real", tienePuerto: false },
+  ciudad14: { nombreCiudad: "Córdoba", tienePuerto: false },
+  ciudad15: { nombreCiudad: "A Coruña", tienePuerto: true },
+  ciudad16: { nombreCiudad: "Cuenca", tienePuerto: false },
+  ciudad17: { nombreCiudad: "Girona", tienePuerto: false },
+  ciudad18: { nombreCiudad: "Granada", tienePuerto: false },
+  ciudad19: { nombreCiudad: "Guadalajara", tienePuerto: false },
+  ciudad20: { nombreCiudad: "Huelva", tienePuerto: true }, // Oeste
+  ciudad21: { nombreCiudad: "Huesca", tienePuerto: false },
+  ciudad22: { nombreCiudad: "Jaén", tienePuerto: false },
+  ciudad23: { nombreCiudad: "León", tienePuerto: false },
+  ciudad24: { nombreCiudad: "Lleida", tienePuerto: false },
+  ciudad25: { nombreCiudad: "Logroño", tienePuerto: false },
+  ciudad26: { nombreCiudad: "Lugo", tienePuerto: false },
+  ciudad27: { nombreCiudad: "Madrid", tienePuerto: false },
+  ciudad28: { nombreCiudad: "Málaga", tienePuerto: false },
+  ciudad29: { nombreCiudad: "Murcia", tienePuerto: false },
+  ciudad30: { nombreCiudad: "Ourense", tienePuerto: false },
+  ciudad31: { nombreCiudad: "Palencia", tienePuerto: false },
+  ciudad32: { nombreCiudad: "Palma", tienePuerto: true }, // Mallorca
+  ciudad33: { nombreCiudad: "Las Palmas de Gran Canaria", tienePuerto: true }, // Gran Canaria
+  ciudad34: { nombreCiudad: "Pontevedra", tienePuerto: false },
+  ciudad35: { nombreCiudad: "Salamanca", tienePuerto: false },
+  ciudad36: { nombreCiudad: "San Sebastián", tienePuerto: false },
+  ciudad37: { nombreCiudad: "Santa Cruz de Tenerife", tienePuerto: true }, // Tenerife
+  ciudad38: { nombreCiudad: "Santander", tienePuerto: true }, // Norte
+  ciudad39: { nombreCiudad: "Segovia", tienePuerto: false },
+  ciudad40: { nombreCiudad: "Sevilla", tienePuerto: false },
+  ciudad41: { nombreCiudad: "Soria", tienePuerto: false },
+  ciudad42: { nombreCiudad: "Tarragona", tienePuerto: false },
+  ciudad43: { nombreCiudad: "Teruel", tienePuerto: false },
+  ciudad44: { nombreCiudad: "Toledo", tienePuerto: false },
+  ciudad45: { nombreCiudad: "Valencia", tienePuerto: true }, // Este
+  ciudad46: { nombreCiudad: "Valladolid", tienePuerto: false },
+  ciudad47: { nombreCiudad: "Zamora", tienePuerto: false },
+  ciudad48: { nombreCiudad: "Zaragoza", tienePuerto: false },
+  ciudad49: { nombreCiudad: "Ceuta", tienePuerto: false },
+  ciudad50: { nombreCiudad: "Melilla", tienePuerto: false },
+};
+
+/**
+ * ? CIUDADES CON PUERTO:
+ * ?
+ * ? Santander (Norte)
+ * ? Cádiz (Sur)
+ * ? Valencia (Este)
+ * ? A Coruña (Oeste)
+ *
+ * ? Huelva - RECIBE EL TRANSPORTE MARITIMO DE LAS CANARIAS
+ * ? Palma (Mallorca)
+ * ? Santa Cruz de Tenerife (Tenerife)
+ * ? Las Palmas de Gran Canaria (Gran Canaria)
+ * ?
+ * ?
+ */
