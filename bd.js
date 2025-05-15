545adbea8182fd2e7a310c4f04d756102695fcdf
adrianbr_Global <adrianbr@global.com>
2025-05-16 00:43:48 +0200

    added bd

---
+++ bd.js
@@ -0,0 +1,324 @@
+export const ciudades = {
+  ciudad1: {
+    nombreCiudad: "Vitoria-Gasteiz",
+    tienePuerto: false,
+    distanciaAMadrid: 351,
+  },
+  ciudad2: {
+    nombreCiudad: "Albacete",
+    tienePuerto: false,
+    distanciaAMadrid: 252,
+  },
+  ciudad3: {
+    nombreCiudad: "Alicante",
+    tienePuerto: false,
+    distanciaAMadrid: 420,
+  },
+  ciudad4: {
+    nombreCiudad: "Almería",
+    tienePuerto: false,
+    distanciaAMadrid: 541,
+  },
+  ciudad5: {
+    nombreCiudad: "Oviedo",
+    tienePuerto: false,
+    distanciaAMadrid: 447,
+  },
+  ciudad6: {
+    nombreCiudad: "Ávila",
+    tienePuerto: false,
+    distanciaAMadrid: 113,
+  },
+  ciudad7: {
+    nombreCiudad: "Badajoz",
+    tienePuerto: false,
+    distanciaAMadrid: 400,
+  },
+  ciudad8: {
+    nombreCiudad: "Barcelona",
+    tienePuerto: false,
+    distanciaAMadrid: 608,
+  },
+  ciudad9: {
+    nombreCiudad: "Burgos",
+    tienePuerto: false,
+    distanciaAMadrid: 214,
+  },
+  ciudad10: {
+    nombreCiudad: "Cáceres",
+    tienePuerto: false,
+    distanciaAMadrid: 298,
+  },
+  ciudad11: {
+    nombreCiudad: "Cádiz",
+    tienePuerto: true,
+    distanciaAMadrid: 621,
+    distanciasConPuerto: {
+      "A Coruña": 955,
+      Huelva: 120,
+      Palma: 1000,
+      "Las Palmas de Gran Canaria": 1400,
+      "Santa Cruz de Tenerife": 1500,
+      Santander: 850,
+      Valencia: 700,
+    },
+  },
+  ciudad12: {
+    nombreCiudad: "Castellón de la Plana",
+    tienePuerto: false,
+    distanciaAMadrid: 408,
+  },
+  ciudad13: {
+    nombreCiudad: "Ciudad Real",
+    tienePuerto: false,
+    distanciaAMadrid: 185,
+  },
+  ciudad14: {
+    nombreCiudad: "Córdoba",
+    tienePuerto: false,
+    distanciaAMadrid: 391,
+  },
+  ciudad15: {
+    nombreCiudad: "A Coruña",
+    tienePuerto: true,
+    distanciaAMadrid: 593,
+    distanciasConPuerto: {
+      Cádiz: 955,
+      Huelva: 950,
+      Palma: 1200,
+      "Las Palmas de Gran Canaria": 1800,
+      "Santa Cruz de Tenerife": 1900,
+      Santander: 300,
+      Valencia: 900,
+    },
+  },
+  ciudad16: {
+    nombreCiudad: "Cuenca",
+    tienePuerto: false,
+    distanciaAMadrid: 170,
+  },
+  ciudad17: {
+    nombreCiudad: "Girona",
+    tienePuerto: false,
+    distanciaAMadrid: 700,
+  },
+  ciudad18: {
+    nombreCiudad: "Granada",
+    tienePuerto: false,
+    distanciaAMadrid: 420,
+  },
+  ciudad19: {
+    nombreCiudad: "Guadalajara",
+    tienePuerto: false,
+    distanciaAMadrid: 60,
+  },
+  ciudad20: {
+    nombreCiudad: "Huelva",
+    tienePuerto: true,
+    distanciaAMadrid: 620,
+    distanciasConPuerto: {
+      Cádiz: 120,
+      "A Coruña": 950,
+      Palma: 1100,
+      "Las Palmas de Gran Canaria": 1400,
+      "Santa Cruz de Tenerife": 1500,
+      Santander: 850,
+      Valencia: 700,
+    },
+  },
+  ciudad21: {
+    nombreCiudad: "Huesca",
+    tienePuerto: false,
+    distanciaAMadrid: 400,
+  },
+  ciudad22: { nombreCiudad: "Jaén", tienePuerto: false, distanciaAMadrid: 350 },
+  ciudad23: { nombreCiudad: "León", tienePuerto: false, distanciaAMadrid: 330 },
+  ciudad24: {
+    nombreCiudad: "Lleida",
+    tienePuerto: false,
+    distanciaAMadrid: 500,
+  },
+  ciudad25: {
+    nombreCiudad: "Logroño",
+    tienePuerto: false,
+    distanciaAMadrid: 330,
+  },
+  ciudad26: { nombreCiudad: "Lugo", tienePuerto: false, distanciaAMadrid: 500 },
+  ciudad27: { nombreCiudad: "Madrid", tienePuerto: false, distanciaAMadrid: 0 },
+  ciudad28: {
+    nombreCiudad: "Málaga",
+    tienePuerto: false,
+    distanciaAMadrid: 530,
+  },
+  ciudad29: {
+    nombreCiudad: "Murcia",
+    tienePuerto: false,
+    distanciaAMadrid: 400,
+  },
+  ciudad30: {
+    nombreCiudad: "Ourense",
+    tienePuerto: false,
+    distanciaAMadrid: 470,
+  },
+  ciudad31: {
+    nombreCiudad: "Palencia",
+    tienePuerto: false,
+    distanciaAMadrid: 230,
+  },
+  ciudad32: {
+    nombreCiudad: "Palma",
+    tienePuerto: true,
+    distanciaAMadrid: 700,
+    distanciasConPuerto: {
+      Cádiz: 1000,
+      "A Coruña": 1200,
+      Huelva: 1100,
+      "Las Palmas de Gran Canaria": 2000,
+      "Santa Cruz de Tenerife": 2100,
+      Santander: 900,
+      Valencia: 300,
+    },
+  },
+  ciudad33: {
+    nombreCiudad: "Las Palmas de Gran Canaria",
+    tienePuerto: true,
+    distanciaAMadrid: 1800,
+    distanciasConPuerto: {
+      Cádiz: 1400,
+      "A Coruña": 1800,
+      Huelva: 1400,
+      Palma: 2000,
+      "Santa Cruz de Tenerife": 100,
+      Santander: 2000,
+      Valencia: 2100,
+    },
+  },
+  ciudad34: {
+    nombreCiudad: "Pontevedra",
+    tienePuerto: false,
+    distanciaAMadrid: 600,
+  },
+  ciudad35: {
+    nombreCiudad: "Salamanca",
+    tienePuerto: false,
+    distanciaAMadrid: 210,
+  },
+  ciudad36: {
+    nombreCiudad: "San Sebastián",
+    tienePuerto: false,
+    distanciaAMadrid: 450,
+  },
+  ciudad37: {
+    nombreCiudad: "Santa Cruz de Tenerife",
+    tienePuerto: true,
+    distanciaAMadrid: 1900,
+    distanciasConPuerto: {
+      Cádiz: 1500,
+      "A Coruña": 1900,
+      Huelva: 1500,
+      Palma: 2100,
+      "Las Palmas de Gran Canaria": 100,
+      Santander: 2100,
+      Valencia: 2200,
+    },
+  },
+  ciudad38: {
+    nombreCiudad: "Santander",
+    tienePuerto: true,
+    distanciaAMadrid: 339,
+    distanciasConPuerto: {
+      Cádiz: 850,
+      "A Coruña": 300,
+      Huelva: 850,
+      Palma: 900,
+      "Las Palmas de Gran Canaria": 2000,
+      "Santa Cruz de Tenerife": 2100,
+      Valencia: 700,
+    },
+  },
+  ciudad39: {
+    nombreCiudad: "Segovia",
+    tienePuerto: false,
+    distanciaAMadrid: 90,
+  },
+  ciudad40: {
+    nombreCiudad: "Sevilla",
+    tienePuerto: false,
+    distanciaAMadrid: 530,
+  },
+  ciudad41: {
+    nombreCiudad: "Soria",
+    tienePuerto: false,
+    distanciaAMadrid: 230,
+  },
+  ciudad42: {
+    nombreCiudad: "Tarragona",
+    tienePuerto: false,
+    distanciaAMadrid: 500,
+  },
+  ciudad43: {
+    nombreCiudad: "Teruel",
+    tienePuerto: false,
+    distanciaAMadrid: 300,
+  },
+  ciudad44: {
+    nombreCiudad: "Toledo",
+    tienePuerto: false,
+    distanciaAMadrid: 75,
+  },
+  ciudad45: {
+    nombreCiudad: "Valencia",
+    tienePuerto: true,
+    distanciaAMadrid: 350,
+    distanciasConPuerto: {
+      Cádiz: 700,
+      "A Coruña": 900,
+      Huelva: 700,
+      Palma: 300,
+      "Las Palmas de Gran Canaria": 2100,
+      "Santa Cruz de Tenerife": 2200,
+      Santander: 700,
+    },
+  },
+  ciudad46: {
+    nombreCiudad: "Valladolid",
+    tienePuerto: false,
+    distanciaAMadrid: 200,
+  },
+  ciudad47: {
+    nombreCiudad: "Zamora",
+    tienePuerto: false,
+    distanciaAMadrid: 250,
+  },
+  ciudad48: {
+    nombreCiudad: "Zaragoza",
+    tienePuerto: false,
+    distanciaAMadrid: 320,
+  },
+  ciudad49: {
+    nombreCiudad: "Ceuta",
+    tienePuerto: false,
+    distanciaAMadrid: 700,
+  },
+  ciudad50: {
+    nombreCiudad: "Melilla",
+    tienePuerto: false,
+    distanciaAMadrid: 700,
+  },
+};
+
+/**
+ * ? CIUDADES CON PUERTO:
+ * ?
+ * ? Santander (Norte)
+ * ? Cádiz (Sur)
+ * ? Valencia (Este)
+ * ? A Coruña (Oeste)
+ *
+ * ? Huelva - RECIBE EL TRANSPORTE MARITIMO DE LAS CANARIAS
+ * ? Palma (Mallorca)
+ * ? Santa Cruz de Tenerife (Tenerife)
+ * ? Las Palmas de Gran Canaria (Gran Canaria)
+ * ?
+ * ?
+ */
